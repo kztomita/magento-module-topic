ebfe9db476f6ee35373c2c7d8a719137ebaee37f
Kazuyoshi Tomita <kztomita@bit-hive.com>
2018-11-30 14:51:53 +0900

    インデント修正

--- Model/ResourceModel/Post/Collection.php
+++ Model/ResourceModel/Post/Collection.php
@@ -3,9 +3,9 @@ namespace BitHive\Topic\Model\ResourceModel\Post;
 
 class Collection extends \Magento\Framework\Model\ResourceModel\Db\Collection\AbstractCollection
 {
-	protected $_idFieldName = 'post_id';
-	protected $_eventPrefix = 'bithive_topic_post_collection';
-	protected $_eventObject = 'post_collection';
+    protected $_idFieldName = 'post_id';
+    protected $_eventPrefix = 'bithive_topic_post_collection';
+    protected $_eventObject = 'post_collection';
 
     /**
      * Define resource model
